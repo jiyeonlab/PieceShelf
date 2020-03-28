9907e6e9f1cca863cb6356e112e3d59dd6b0bc99
jiyeonlab <jiyeonlab@gmail.com>
2020-03-28 14:47:17 +0900

    [Feat] AddVC에 ActionSheet 추가 및 WebSearchVC 추가
    AddVC의 ActionSheet에 웹 검색, 카메라 접근, 앨범 접근 3가지 액션 추가.
    WebVC에는 UISearchBar 포함.

--- PieceShelf/AddViewController.swift
+++ PieceShelf/AddViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showActionSheet(_:)))
+        imageView.addGestureRecognizer(tapGesture)
     }
     
     @IBAction func tappedBack(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func showActionSheet(_ sender: UITapGestureRecognizer){
+        let alert = UIAlertController()
+        
+        let webSearch = UIAlertAction(title: "웹에서 검색하기", style: .default){ _ in
+            guard let webSearchVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "WebSearchVC") as? WebSearchViewController else {
+                return
+            }
+            
+            webSearchVC.modalPresentationStyle = .fullScreen
+            self.present(webSearchVC, animated: true)
+        }
+        let albumSearch = UIAlertAction(title: "앨범에서 가져오기", style: .default, handler: nil)
+        let camera = UIAlertAction(title: "카메라로 촬영하기", style: .default, handler: nil)
+        
+        alert.addAction(webSearch)
+        alert.addAction(albumSearch)
+        alert.addAction(camera)
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        
+        present(alert, animated: true)
     }
 }
---
+++ PieceShelf/WebSearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WebSearchViewController.swift
+//  PieceShelf
+//
+//  Created by JiyeonKim on 2020/03/28.
+//  Copyright © 2020 jiyeonlab. All rights reserved.
+//
+
+import UIKit
+
+class WebSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tappedBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
