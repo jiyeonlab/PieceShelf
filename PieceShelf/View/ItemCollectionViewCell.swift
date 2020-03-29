3530c32ea3970ea35a8c6c16ede588635411c403
jiyeonlab <jiyeonlab@gmail.com>
2020-03-30 01:11:33 +0900

    [Feat] 메인 화면의 카테고리별 collection view 추가 및 설정

---
+++ PieceShelf/View/ItemCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCollectionViewCell.swift
+//  PieceShelf
+//
+//  Created by JiyeonKim on 2020/03/30.
+//  Copyright © 2020 jiyeonlab. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "ItemCollectionViewCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- PieceShelf/View/MainSectionTableViewCell.swift
+++ PieceShelf/View/MainSectionTableViewCell.swift
@@ -16,9 +16,16 @@ class MainSectionTableViewCell: UITableViewCell {
     // presentDelegate 변수
     var presentDelegate: PresentDelegate?
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        collectionView.dataSource = self
+        
+        let nibName = UINib(nibName: "ItemCollectionViewCell", bundle: nil)
+        collectionView.register(nibName, forCellWithReuseIdentifier: ItemCollectionViewCell.identifier)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,6 +38,19 @@ class MainSectionTableViewCell: UITableViewCell {
 
         guard let catecory = catecoryName.text else { return }
         presentDelegate?.loadNewVC(by: catecory)
+    }
+    
+}
+
+extension MainSectionTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemCollectionViewCell.identifier, for: indexPath)
+        
+        return cell
     }
     
 }
